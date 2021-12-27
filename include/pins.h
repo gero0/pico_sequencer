f911bc2d7a9ccbd3cdc857c765cf9268900c745c
Kacper Leśniański <kacper.lesnianski@wp.pl>
2021-12-28 00:42:01 +0100

    Initial commit

---
+++ include/pins.h
@@ -0,0 +1,55 @@
+#ifndef __PINS__
+#define __PINS__
+
+#include "pico/stdlib.h"
+#include "bsp/board.h"
+
+//Define button matrix column and row pins
+#define COL0 4
+#define COL1 5
+#define COL2 6
+#define COL3 7
+
+#define ROW0 8
+#define ROW1 9
+#define ROW2 10
+#define ROW3 11
+
+//Define LED shift registers pins
+
+#define SHIFT_DATA 12
+#define CLOCK 13
+#define LATCH 14
+
+//Tempo LED
+#define TEMPO_LED 15
+
+//Other buttons
+#define CLEAR_BTN 16
+#define SET_BTN 17
+#define START_STOP_BTN 18
+#define TEST_BTN 19
+
+//Encoder pins
+#define ENC_1 27
+#define ENC_2 28
+
+//Shift register for driving HD44780
+#define DISPLAY_DATA 20
+#define DISPLAY_CLOCK 21
+#define DISPLAY_LATCH 22
+
+//Reserved
+//0,1,2,3
+//EEPROM for settings?
+//UART for MIDI
+
+//Velocity potentiometer (ADC0)
+#define VEL_POT 26
+
+//Free
+//27, 28
+
+void configure_pins();
+
+#endif
---
+++ src/pins.c
@@ -0,0 +1,78 @@
+#include "pins.h"
+
+void configure_pins(){
+    //Matrix
+
+    gpio_init(COL0);
+    gpio_set_dir(COL0, GPIO_OUT);
+
+    gpio_init(COL1);
+    gpio_set_dir(COL1, GPIO_OUT);
+
+    gpio_init(COL2);
+    gpio_set_dir(COL2, GPIO_OUT);
+
+    gpio_init(COL3);
+    gpio_set_dir(COL3, GPIO_OUT);
+
+    gpio_init(ROW0);
+    gpio_set_dir(ROW0, GPIO_IN);
+    gpio_pull_down(ROW0);
+
+    gpio_init(ROW1);
+    gpio_set_dir(ROW1, GPIO_IN);
+    gpio_pull_down(ROW1);
+
+    gpio_init(ROW2);
+    gpio_set_dir(ROW2, GPIO_IN);
+    gpio_pull_down(ROW2);
+
+    gpio_init(ROW3);
+    gpio_set_dir(ROW3, GPIO_IN);
+    gpio_pull_down(ROW3);
+
+    //LED SR
+    gpio_init(SHIFT_DATA);
+    gpio_set_dir(SHIFT_DATA, GPIO_OUT);
+
+    gpio_init(LATCH);
+    gpio_set_dir(LATCH, GPIO_OUT);
+    
+    gpio_init(CLOCK);
+    gpio_set_dir(CLOCK, GPIO_OUT);
+
+    //Tempo LED
+    gpio_init(TEMPO_LED);
+    gpio_set_dir(TEMPO_LED, GPIO_OUT);
+
+    //Other buttons
+    gpio_init(CLEAR_BTN);
+    gpio_set_dir(CLEAR_BTN, GPIO_IN);
+    gpio_pull_down(CLEAR_BTN);
+
+    gpio_init(SET_BTN);
+    gpio_set_dir(SET_BTN, GPIO_IN);
+    gpio_pull_down(SET_BTN);
+
+    gpio_init(TEST_BTN);
+    gpio_set_dir(TEST_BTN, GPIO_IN);
+    gpio_pull_down(TEST_BTN);
+
+    gpio_init(START_STOP_BTN);
+    gpio_set_dir(START_STOP_BTN, GPIO_IN);
+    gpio_pull_down(START_STOP_BTN);
+
+    //Encoder pins
+    // ??
+
+    //Display SR
+    gpio_init(DISPLAY_DATA);
+    gpio_set_dir(DISPLAY_DATA, GPIO_OUT);
+
+    gpio_init(DISPLAY_LATCH);
+    gpio_set_dir(DISPLAY_LATCH, GPIO_OUT);
+    
+    gpio_init(DISPLAY_CLOCK);
+    gpio_set_dir(DISPLAY_CLOCK, GPIO_OUT);
+
+}
