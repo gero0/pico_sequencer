be6732f68d316ed3c9a2bed173cd5ad7a978f487
Kacper Leśniański <kacper.lesnianski@wp.pl>
2022-01-03 20:30:14 +0100

    remap pins

--- include/pins.h
+++ include/pins.h
@@ -1,48 +1,41 @@
 #ifndef __PINS__
 #define __PINS__
 
-//Define button matrix column and row pins
+//Reserved
+//0,1
+//UART for MIDI
+//2,3  I2C DAC
+//8-11 SPI SD card for settings & samples
 
-//Free 7,8,9,10,11
-
+//Step buttons
 #define COL_DATA 4
 #define COL_CLOCK 5
-
 #define ROW0 6
 
-//Define LED shift registers pins
+//Tempo LED
+#define TEMPO_LED 7
 
+//Define LED shift registers pins
 #define SHIFT_DATA 12
 #define CLOCK 13
 #define LATCH 14
 
-//Tempo LED
-#define TEMPO_LED 15
-
-//Other buttons
+//Control buttons
+#define SETTING_BTN 15
 #define CLEAR_BTN 16
 #define SET_BTN 17
 #define START_STOP_BTN 18
 #define TEST_BTN 19
+#define HOLD_BTN 20
+
+//Shift register for driving HD44780
+#define DISPLAY_DATA 21
+#define DISPLAY_CLOCK 22
+#define DISPLAY_E 26
 
 //Encoder pins
 #define ENC_1 27
 #define ENC_2 28
-
-//Shift register for driving HD44780
-#define DISPLAY_DATA 20
-#define DISPLAY_CLOCK 21
-#define DISPLAY_E 22
-
-//Hold button
-#define HOLD_BTN 26
-
-
-//Reserved
-//0,1,2,3
-//EEPROM for settings?
-//UART for MIDI
-
 
 void configure_pins();
 
