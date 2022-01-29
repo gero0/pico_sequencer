e995d806982a7d7577dff5992f92d23d2011c310
Kacper Leśniański <kacper.lesnianski@wp.pl>
2022-01-30 00:57:51 +0100

    remap buttons

--- include/pins.h
+++ include/pins.h
@@ -21,12 +21,12 @@
 #define LATCH 14
 
 //Control buttons
-#define SETTING_BTN 15
-#define CLEAR_BTN 16
-#define SET_BTN 17
-#define START_STOP_BTN 18
+#define START_STOP_BTN 15
+#define SET_BTN 20
 #define TEST_BTN 19
-#define HOLD_BTN 20
+#define CLEAR_BTN 18
+#define HOLD_BTN 17
+#define SETTING_BTN 16
 
 //Shift register for driving HD44780
 #define DISPLAY_DATA 21
